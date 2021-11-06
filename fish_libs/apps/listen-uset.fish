3ef02df3e481641f295f07f5cf19a78aaa03d171
tsingkwai <tsingkwai@bluelaketable>
2021-11-06 14:21:35 +0800

    2021-11-06_06:21:35

--- fish_libs/apps/listen-uset.fish
+++ fish_libs/apps/listen-uset.fish
@@ -1,5 +1,5 @@
 function listen-uset -d "Saeialbarid api socket server"
-set -g saeialbaridsocketaddr
+set -g saeialbaridsocketaddr $argv[1]
 if [ "$argv[1]" = "" ]
   set -g saeialbaridsocketaddr '/tmp/saeialbarid.sock'
 end
